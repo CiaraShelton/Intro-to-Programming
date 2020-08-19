722f16e62cb35a3b2c4c654a325d0f7375605069
CiaraShelton <69883129+CiaraShelton@users.noreply.github.com>
2020-08-18 22:41:19 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/***************************************************************
+* Hello Java
+****************************************************************
+* Ciara Shelton
+* August 18, 2020
+* 255 C90
+***************************************************************/
+
+public class Hello {
+
+	public static void main(String[] args) {
+	System.out.println("<Ciara Shelton>");
+	System.out.println("<Hello Java>");
+	System.out.println("<Creating a small program using Java>");
+	System.out.println("<October 1, 2019>");
+	System.out.println("<255 C90>");
+	System.out.println();
+	}
+}
